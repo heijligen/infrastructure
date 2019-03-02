a1c4ab2a90d78be1dc80b47d4c37fd8b8ff4d906
Thomas Heijligen <src@posteo.de>
2019-03-02 18:13:42 +0100

    adsf

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ NAME=$1
 parted /dev/sda -- mklabel msdos
 parted /dev/sda -- mkpart primary 1 -1
 
-mkfs.xfs /dev/sda1
+mkfs.xfs -f /dev/sda1
 mount /dev/sda1 /mnt
 mkdir /mnt/etc
 
 git clone https://github.com/heijligen/infrastructure /mnt/etc/nixos
-ln -s /etc/nixos/$NAME.nix /etc/nixos/configuration.nix
+ln -s /mnt/etc/nixos/$NAME.nix /mnt/etc/nixos/configuration.nix
